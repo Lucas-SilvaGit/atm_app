840aa1b8abf591542dcc7d007d49cba3988a312c
Jose Lucas <lucasnba2324@gmail.com>
2022-10-27 20:50:29 -0300

    created method to close account by current_user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   def close_account
-    current_user.update(inactive_at: Time.now)
+    current_user.update(inactive_at: Time.now, password: current_user.encrypted_password)
+    redirect_to root_url 
   end
 end
