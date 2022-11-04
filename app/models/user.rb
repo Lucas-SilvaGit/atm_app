d01e45c0c55b9545f5690af916dff09b1711ae65
Jose Lucas <lucasnba2324@gmail.com>
2022-11-04 20:28:12 -0300

    created method for deposit

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,23 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :email, :password, presence: true
+  attr_accessor :value
+        validates :email, :password, presence: true
+        # validates :value, numericality: { odd: true }
+
+  def value
+    @value.to_f
+  end
+
+  # def value=(value)
+  #   @value.to_f
+  # end
+
+  def deposit(value) 
+    if value > 0
+      update(balance: self.balance += value, password: 123456)
+    else
+      puts "Please enter a valid value"
+    end
+  end
 end
