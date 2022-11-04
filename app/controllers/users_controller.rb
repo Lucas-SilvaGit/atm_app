38e28325cb7357bdbf962825c98c59786945230f
Jose Lucas <lucasnba2324@gmail.com>
2022-11-04 20:27:18 -0300

    nothing altered

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,4 @@
-class UsersController < ApplicationController
+class UsersController < ApplicationController  
   def close_account
     current_user.update(inactive_at: Time.now, password: current_user.encrypted_password)
     redirect_to root_url 
