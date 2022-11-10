e325b5d8f501e590189c028e1064de719753c8ff
Jose Lucas <lucasnba2324@gmail.com>
2022-11-10 19:11:42 -0300

    created route to action deposit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   get '/users/close_account', to: 'users#close_account', as: 'users'
+  get '/users/deposit_account', to: 'users#deposit_account', as: 'users_deposit'
 
   resources :site
 
