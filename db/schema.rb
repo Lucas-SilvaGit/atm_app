35cd7dbb770716bbaedb5c329c99b8e3e840a98a
Jose Lucas <lucasnba2324@gmail.com>
2022-11-10 19:11:57 -0300

    nothing

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_12_202116) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_07_235443) do
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
