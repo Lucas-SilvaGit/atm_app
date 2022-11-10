738b2f3bbd0a3145481af0ef118c50310fc17072
Jose Lucas <lucasnba2324@gmail.com>
2022-11-10 19:10:58 -0300

    created method deposit_account

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,9 @@
 class UsersController < ApplicationController  
   def close_account
-    current_user.update(inactive_at: Time.now, password: current_user.encrypted_password)
+    current_user.update(inactive_at: Time.now, password: 123456) #current_user.encrypted_password
     redirect_to root_url 
+  end
+
+  def deposit_account
   end
 end
