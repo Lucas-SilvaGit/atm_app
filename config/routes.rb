4430abe43bdf6fe247b5a1c020ee9c7be83c470c
Jose Lucas <lucasnba2324@gmail.com>
2022-11-14 00:33:43 -0300

    created route post to method deposit

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   get '/users/close_account', to: 'users#close_account', as: 'users'
   get '/users/deposit_account', to: 'users#deposit_account', as: 'users_deposit'
+  post '/users/deposit', to: 'users#deposit'
 
   resources :site
 
